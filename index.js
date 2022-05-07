198a69845ce88bbcc4e9a330f751ae25268a79d2
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-08 04:25:01 +0600

    added single add item api and init jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// JWT
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
+    req.decoded = decoded;
+    console.log('decoded', decoded);
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cycle.iuhgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -28,6 +46,17 @@ async function run() {
       const cursor = cycleCollection.find(query);
       const cycles = await cursor.toArray();
       res.send(cycles);
+    });
+
+    // Add one cycle
+    app.post('/cycle', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const doc = {
+        ...item,
+      };
+      const result = await cycleCollection.insertOne(doc);
+      res.send(result);
     });
 
     // Cycle By ID
